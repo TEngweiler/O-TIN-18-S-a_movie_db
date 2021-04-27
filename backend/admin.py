009ecacc4f79746e3be546b23ec70f7a51702f4f
joenatan <j.brutschin@gmail.com>
2021-04-27 20:14:44 +0200

    init admin classes

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+# import von den Modelsklassen
+from .models import Movie, Genre
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
