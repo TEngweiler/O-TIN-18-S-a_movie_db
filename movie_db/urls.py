58a30ea0a91d9596af50aedca94df5d140397de8
jonathan <j.brutschin@gmail.com>
2021-05-11 20:33:42 +0200

    add jqt token auth

--- movie_db/urls.py
+++ movie_db/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework import permissions
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -35,6 +36,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('api/v1/', include('backend.urls')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/schema.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('api/v1/doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-ui'),
     ##path('api/v1/doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
